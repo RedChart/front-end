76e31299fec0450ce050fcf7ce2437bb7d315382
seiyeon <wsx4379@naver.com>
2024-08-20 00:13:33 +0900

    style: 공통 color 추가
    공통 color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,29 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      main: {
+        DEFAULT: "#f86978",
+        light: "#ffe3e6",
+      },
+      point: {
+        DEFAULT: "#6979f8",
+        light: "#e7e9fe",
+        yellow: "f8e969",
+        orange: "f8a269",
+      },
+      background: "#fafafa",
+      white: "#ffffff",
+      gray: {
+        200: "#eeeeee",
+        300: "#e0e0e0",
+        400: "#bdbdbd",
+        500: "#9e9e9e",
+        600: "#757575",
+        700: "#616161",
+        900: "#212121",
+      },
+    },
   },
   plugins: [],
 };
